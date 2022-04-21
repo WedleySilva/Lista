7c614bdbfdc458c9fe07bf1bf7dfb28ca5ace826
Aqua <99206531+WedleySilva@users.noreply.github.com>
2022-04-21 16:20:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-.container
+function addTask() {
+    const newTask = document.getElementsByClassName('new-task')[0].value 
+    if (newTask ==='') {return}
+    const $ul = document.getElementsByTagName('ul')[0]
+    const $li = document.createElement('li')
+    const $div = document.creatElement('div')
+    $div.ClassName += 'checkbox'
+    const $label = document.creatElement('label')
+    const $input = document.creatElement('input')
+    $input.className += 'task-item'
+    $input.setAtibute('type', 'checkbox')
+    $input.setAtibute('name', 'tarefa')
+    $input.addEventListener('click', toggleRemoved)
+    $label.appendChild($input)
+    $label.appendChild(document.createTextNode(newTask))
+    $div.appendChild($label)
+    $li.appendChild($div)
+    $ul.appendChild($li)
+    document.getElementsByClassName('newTask')[0].value = ''
+
+}
+
+function toggleRemoved(e) {
+    const $li = e.currentTarget.closest('li')
+    $li.ClassList.toggle('removed')
+}
+
+document.getElementsByClassName('add-task')[0].addEventListener('click', addTask)
+
+const tasks = document.getElementsByClassName('task-item')
+
+Array.from(tasks).forEach(task => task.addEventListener('click', toggleRemoved))
